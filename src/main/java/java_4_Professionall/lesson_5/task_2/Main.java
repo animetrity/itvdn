334c5879e67f1718ed9b4df119c7f74d5eac46b1
neregist <animetrity@gmail.com>
2023-02-13 19:01:54 +0200

    019

---
+++ src/main/java/java_4_Professionall/lesson_5/task_2/Main.java
@@ -0,0 +1,28 @@
+package main.java.java_4_Professionall.lesson_5.task_2;
+
+import java.lang.reflect.Member;
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	try {
+		System.out.println("Введіть назву класу:");
+		Class clazz = Class.forName(new Scanner(System.in).nextLine());/*java.lang.String*/
+		System.out.println(clazz);
+		printInfo(clazz.getFields());
+		printInfo(clazz.getConstructors());
+		printInfo(clazz.getMethods());
+	} catch (ClassNotFoundException e) {
+		System.out.println("Невідомий клас:" + args[0]);
+	}
+}
+private static void printInfo(Member[] members) {
+	for (int i = 0; i < members.length; i++) {
+		if (members[i].getDeclaringClass() == Object.class)
+			continue;
+		String decl = members[i].toString();
+		System.out.print("   ");
+		System.out.println(decl);
+	}
+}
+}
