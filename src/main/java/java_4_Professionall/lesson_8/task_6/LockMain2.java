1f14c4465042168e644b0f9e57146aa1a92c7ecc
neregist <animetrity@gmail.com>
2023-02-22 18:49:27 +0200

    020

--- src/main/java/java_4_Professionall/lesson_8/task_6/LockMain2.java
+++ src/main/java/java_4_Professionall/lesson_8/task_6/LockMain2.java
@@ -6,18 +6,53 @@ import java.util.concurrent.Semaphore;
  * тут використано Semaphore для уникнення взаємного блокування
  */
 public class LockMain2 {
-}
-class First extends Thread{
-private Semaphore semaphore = new Semaphore(1);
-@Override
-public void run(){
+public static void main(String[] args) {
+	Semaphore semaphore = new Semaphore(1);
 
-}
-}
-class Second extends Thread{
-private Semaphore semaphore = new Semaphore(1);
-@Override
-public void run(){
+	FirstSemaphoreThread firstThread = new FirstSemaphoreThread(semaphore);
+	SecondSemaphoreThread secondThread = new SecondSemaphoreThread(semaphore);
 
+	firstThread.start();
+	secondThread.start();
+}
+}
+class FirstSemaphoreThread extends Thread {
+private final Semaphore semaphore;
+
+public FirstSemaphoreThread(Semaphore semaphore) {
+	this.semaphore = semaphore;
+}
+
+@Override
+public void run() {
+	System.out.println("FirstThread is start");
+	try {
+		semaphore.acquire();
+		Thread.sleep(1000);
+		semaphore.release();
+	} catch (InterruptedException e) {
+		Thread.currentThread().interrupt();
+		e.printStackTrace();
+	}
+	System.out.println("FirstThread is finish");
+}
+}
+class SecondSemaphoreThread extends Thread {
+private final Semaphore semaphore;
+public SecondSemaphoreThread(Semaphore semaphore) {
+	this.semaphore = semaphore;
+}
+@Override
+public void run() {
+	System.out.println("SecondThread is start");
+	try {
+		semaphore.acquire();
+		Thread.sleep(1000);
+		semaphore.release();
+	} catch (InterruptedException e) {
+		Thread.currentThread().interrupt();
+		e.printStackTrace();
+	}
+	System.out.println("SecondThread is finish");
 }
 }
