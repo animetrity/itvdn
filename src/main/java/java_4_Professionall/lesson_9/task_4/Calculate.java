3edb9a8bbf23247ad8b14b4b9699d186d83ad622
neregist <animetrity@gmail.com>
2023-02-24 12:41:24 +0200

    021

--- src/main/java/java_4_Professionall/lesson_9/task_4/Calculate.java
+++ src/main/java/java_4_Professionall/lesson_9/task_4/Calculate.java
@@ -2,9 +2,21 @@ package java_4_Professionall.lesson_9.task_4;
 
 public class Calculate {
 	public static void main(String[] args) {
+		Calc add = (a,b) -> a + b;
+		Calc sub = (a,b) -> a - b;
+		Calc mul = (a,b) -> a * b;
+		Calc div = (a,b) -> (double) a / b;
+
+		int a = 5;
+		int b = 10;
+
+		System.out.println("Додавання = " + a + " і " + b + " = " + (int)add.calculate(a,b));
+		System.out.println("Віднімання = " + a + " і " + b + " = " + (int)sub.calculate(a,b));
+		System.out.println("Множення = " + a + " і " + b + " = " + (int)mul.calculate(a,b));
+		System.out.println("Ділення = " + a + " і " + b + " = " + div.calculate(a,b));
 
 	}
 }
 interface Calc {
-	int calculate(int x, int y);
+	double calculate(int a, int b);
 }
